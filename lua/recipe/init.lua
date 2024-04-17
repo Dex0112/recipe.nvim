688aae160a6e4193b3526414101a43c14665c6e6
Dex0112 <dexcrazy06@gmail.com>
2024-04-16 20:22:41 -0400

    FEAT: Popup menu

--- lua/recipe/init.lua
+++ lua/recipe/init.lua
@@ -1,50 +1,93 @@
+local popup = require("plenary.popup")
 local M = {}
 
+local popup_bufnr = nil
+local popup_win_id = nil
+
+local todo_elements = {
+    "Your mom",
+    "Your dad",
+    "Finish coding",
+    "Rewrite universe",
+}
+
 -- Do these need to go in module?
-local bufnr = nil
-local win = nil
-local augroup = vim.api.nvim_create_augroup('TodoList', { clear = true })
+-- local win_id = nil
+-- local augroup = vim.api.nvim_create_augroup('TodoList', { clear = true })
 
-local timerEnd = true
 
---TODO: Add saving behaviour based on working directory for easy pickup
-
-M.setup = function(opts)
+--TODO: Add saving behaviour based on working directory for easy pip
+--
+-- ● ball
+-- { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+function M.setup(opts)
     -- Set default mappings or whatnot
     -- A opt for that takes a function that decides how the todo opens
     -- Ex: time
+
     opts = opts or {
-        time = 10 * 60 * 1000,
-        open_todo = function()
-            vim.cmd('rightbelow vsplit')
-
-            win = vim.api.nvim_get_current_win()
-
-            vim.api.nvim_win_set_buf(win, bufnr)
-        end,
+        time = 10 * 1000,
     }
-
-    bufnr = vim.api.nvim_create_buf(false, true)
-
-    M.open_todo = opts['open_todo']
-
-    vim.api.nvim_create_autocmd('BufWinLeave', {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-            if timerEnd then
-                timerEnd = false
-                vim.defer_fn(function()
-                    M.open_todo()
-                    timerEnd = true
-                end, opts['time'])
-            end
-        end
-    })
 end
 
-M.end_todo = function()
+function M.end_todo()
+end
 
+function M.toggle_menu()
+    if popup_win_id ~= nil and vim.api.nvim_win_is_valid(popup_win_id) then
+        -- close popup
+        vim.api.nvim_win_close(popup_win_id, true)
+
+        popup_win_id = nil
+        popup_bufnr = nil
+        return
+    end
+
+    local popup_window = M.create_overview()
+    popup_win_id = popup_window.win_id
+    popup_bufnr = popup_window.bufnr
+
+    for i = 0, #todo_elements - 1, 1 do
+        vim.api.nvim_buf_set_lines(popup_bufnr, i, i, false, { "● " .. todo_elements[i + 1] })
+    end
+
+    -- apply keymaps and whatever
+    vim.keymap.set('n', '<CR>', function()
+        local idx = vim.api.nvim_win_get_cursor(popup_win_id)[1]
+        print("You are on line " .. idx)
+    end, { buffer = popup_bufnr })
+
+
+    vim.api.nvim_buf_set_option(popup_bufnr, "modifiable", false)
+end
+
+function M.create_overview()
+    local width = 80
+    local height = 40
+    local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    local bufnr = vim.api.nvim_create_buf(false, true)
+
+
+    local recipe_win_id, win = popup.create(bufnr, {
+        title = "Recipe Overview",
+        highlight = "RecipeWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+    })
+
+    vim.api.nvim_win_set_option(
+        win.border.win_id,
+        "winhl",
+        "Normal:RecipeWindow"
+    )
+
+    return {
+        bufnr = bufnr,
+        win_id = recipe_win_id,
+    }
 end
 
 return M
--- plugin/test.lua
+++ plugin/test.lua
@@ -1,9 +1,2 @@
--- require("recipe")
-
-vim.keymap.set('n', '<leader>vt', function() require('recipe').open_todo() end, {})
-
--- local bufnr = vim.api.nvim_create_buf(false, false)
--- 
--- vim.api.nvim_set_current_buf(bufnr)
--- 
--- print(bufnr)
+local M = require('recipe')
+vim.keymap.set('n', '<leader>vt', M.toggle_menu)
